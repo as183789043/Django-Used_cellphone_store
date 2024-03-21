332ac2023f6517b4cdbcded557a747df1af604f4
Rick Hsu <as123789043@gmail.com>
2024-03-22 01:11:15 +0800

    ADD ER Diagram

--- used_cellphone_store/used_cellphone_store/settings.py
+++ used_cellphone_store/used_cellphone_store/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'mysite'
     
 ]
@@ -130,3 +131,9 @@ MEDIA_ROOT =  BASE_DIR / "media"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# ER Digram
+GRAPH_MODELS = {
+  'all_applications': True,
+  'group_models': True,
+}
